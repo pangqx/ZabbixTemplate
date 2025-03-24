dd7ad3fe744af0e340fbd2b93874028cfb021133
Ethan Paul <lsames24@gmail.com>
2025-03-24 14:27:06 +0800

    db_parallel_tables.sql and db_parallel_indexes.sql added

---
+++ oracle/db_parallel_indexes.sql
@@ -0,0 +1,6 @@
+SET      pagesize 0
+SET      heading OFF
+SET      feedback OFF
+SET	 verify OFF
+select count(*) from dba_indexes where degree > 1 and owner not in ('SYS', 'SYSTEM', 'GSMADMIN_INTERNAL', 'WMSYS', 'XDB', 'DBSNMP', 'APPQOSSYS', 'OUTLN', 'DBSFWUSER', 'AUDSYS');
+QUIT;
