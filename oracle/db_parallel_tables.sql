b857fe5c9d7bd03fdfbf909a57f7c8f5281c2beb
Ethan Paul <lsames24@gmail.com>
2025-04-09 16:17:57 +0800

    sql statement fixed

--- oracle/db_parallel_tables.sql
+++ oracle/db_parallel_tables.sql
@@ -2,5 +2,5 @@ SET      pagesize 0
 SET      heading OFF
 SET      feedback OFF
 SET	 verify OFF
-select count(*) from dba_tables where degree > 1 and owner not in ('SYS', 'SYSTEM', 'GSMADMIN_INTERNAL', 'WMSYS', 'XDB', 'DBSNMP', 'APPQOSSYS', 'OUTLN', 'DBSFWUSER', 'AUDSYS');
+select count(*) from dba_tables where trim(degree) not in ('DEFAULT', '1') and owner not in ('SYS', 'SYSTEM', 'GSMADMIN_INTERNAL', 'WMSYS', 'XDB', 'DBSNMP', 'APPQOSSYS', 'OUTLN', 'DBSFWUSER', 'AUDSYS');
 QUIT;
