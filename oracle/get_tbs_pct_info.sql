dee22dc17dcf2bc194470f5ce9a254251591c0fc
Ethan Paul <lsames24@gmail.com>
2025-07-11 16:17:29 +0800

    .

--- oracle/get_tbs_pct_info.sql
+++ oracle/get_tbs_pct_info.sql
@@ -8,11 +8,12 @@ SELECT
         )
     ) AS DATA
 from (
-SELECT a.tablespace_name tablespacename, round(a.current_size - c.free, 1) "USED_SIZE", round(((a.current_size - c.free)*100)/a.max_size, 1) "USED_PCT"
+SELECT a.tablespace_name tablespacename, round(a.current_size - c.free + r.recyclebin_size, 1) "USED_SIZE", round(((a.current_size - c.free + r.recyclebin_size)*100)/a.max_size, 1) "USED_PCT"
 FROM
 (SELECT tablespace_name, SUM(bytes) current_size, SUM(decode(autoextensible, 'NO', bytes, GREATEST(maxbytes, bytes))) max_size FROM dba_data_files x where exists (select 1 from dba_tablespaces y where y.tablespace_name=x.tablespace_name and y.contents='PERMANENT') GROUP BY tablespace_name) a,
-(SELECT d.tablespace_name, sum(nvl(b.bytes,0)) free FROM dba_tablespaces d, dba_free_space b WHERE d.tablespace_name = b.tablespace_name(+) GROUP BY d.tablespace_name) c
-WHERE a.tablespace_name = c.tablespace_name 
+(SELECT d.tablespace_name, sum(nvl(b.bytes,0)) free FROM dba_tablespaces d, dba_free_space b WHERE d.tablespace_name = b.tablespace_name(+) GROUP BY d.tablespace_name) c,
+(select f.tablespace_name, sum(nvl(g.space*f.block_size, 0)) recyclebin_size from dba_tablespaces f, dba_recyclebin g where f.tablespace_name = g.ts_name(+) group by f.tablespace_name) r
+WHERE a.tablespace_name = c.tablespace_name and a.tablespace_name = r.tablespace_name
 union all
 SELECT a.tablespace_name tablespacename, round(a.current_size - c.free, 1) "USED_SIZE", round(((a.current_size - c.free)*100)/a.max_size, 1) "USED_PCT"
 FROM
